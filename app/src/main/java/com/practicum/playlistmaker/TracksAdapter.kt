9476f7198f1e419a2385456357f222f15f428e6d
AnastassiyaFom <a.a.fomenkova@mail.ru>
2025-05-27 17:06:31 +0300

    Homework sprint 10++

--- app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -30,42 +30,5 @@ class TracksAdapter (
     override fun getItemCount() = tracks.size
 }
 
-class TracksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-    private val trackNameView: TextView // Название композиции
-    private  val artistNameView: TextView // Имя исполнителя
-    private val trackTimeView: TextView// Продолжительность трека
-    private val albumImageView: ImageView // Ссылка на изображение обложки
-    private val arrowView: ImageView
-    init {
-        trackNameView = itemView.findViewById(R.id.track_name)
-        artistNameView = itemView.findViewById(R.id.artist_name)
-        trackTimeView = itemView.findViewById(R.id.track_time)
-        albumImageView = itemView.findViewById(R.id.album_image)
-        arrowView = itemView.findViewById(R.id.to_track)
-    }
-    fun bind(model: Track) {
-        trackNameView.text = model.trackName
-        artistNameView.text = model.artistName
-        trackTimeView.text = model.trackTime
-        Glide.with(itemView)
-            .load(model.artworkUrl100)
-            .placeholder(R.drawable.placeholder)
-            .centerCrop()
-            .transform(RoundedCorners(dpToPixel(2f)))
-            .into(albumImageView)
-    }
 
-    private fun dpToPixel(dp: Float): Int {
-        val metrics: DisplayMetrics = Resources.getSystem().getDisplayMetrics()
-        val px = dp * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
-        return Math.round(px).toInt()
-    }
-}
-
-data class Track(
-    val trackName: String,
-    val artistName: String,
-    val trackTime: String,
-    val artworkUrl100: String
-)
 
