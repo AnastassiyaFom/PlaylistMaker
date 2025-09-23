8a461fe4af329653cbd2aea89a2a254b7d55286b
AnastassiyaFom <a.a.fomenkova@mail.ru>
2025-09-23 11:06:05 +0300

    Homework sprint 18, Homework remove

--- app/src/main/java/com/practicum/playlistmaker/library/ui/activity/LibraryActivityNew.kt
+++ app/src/main/java/com/practicum/playlistmaker/library/ui/activity/LibraryActivityNew.kt
@@ -4,19 +4,23 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ActivityMediatechBinding
 import com.practicum.playlistmaker.main.ui.MainActivity
-
+import com.practicum.playlistmaker.player.ui.activity.Playlists
+import com.practicum.playlistmaker.player.ui.activity.SelectedTracks
 
 class LibraryActivityNew: AppCompatActivity()  {
     @SuppressLint("WrongViewCast", "MissingInflatedId")
 
     private lateinit var binding:ActivityMediatechBinding
-    private lateinit var tabMediator: TabLayoutMediator
+    private var fragmentSelectedFlag: FragmentSelectionFlag = FragmentSelectionFlag.SELECTED_TRACKS
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,30 +29,58 @@ class LibraryActivityNew: AppCompatActivity()  {
         binding = ActivityMediatechBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
 
-        setOnClickListeners(this)
-
-        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            when(position) {
-                0 -> tab.setText(R.string.selected_tracks)
-                1 -> tab.setText(R.string.playlists)
-            }
+        if (savedInstanceState == null ) {
+            showSelectedTracks()
         }
-        tabMediator.attach()
+        setOnClickListeners(this)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        tabMediator.detach()
+    fun showSelectedTracks(){
+        binding.selectedTracksLine.visibility = View.VISIBLE
+        binding.playlistsLine.visibility = View.GONE
+        // реализация через вызов методов
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentСontainerView, SelectedTracks())
+            .commit()
     }
-
+    fun showPlaylist(){
+        binding.selectedTracksLine.visibility = View.GONE
+        binding.playlistsLine.visibility = View.VISIBLE
+        // реализация через лямбду
+        supportFragmentManager.commit {
+            add<Playlists>(R.id.fragmentСontainerView)
+        }
+    }
     private fun setOnClickListeners(context: Context) {
+        binding.selectedTracks.setOnClickListener {
+            fragmentSelectedFlag = FragmentSelectionFlag.SELECTED_TRACKS
+            binding.selectedTracksLine.visibility = View.VISIBLE
+            binding.playlistsLine.visibility = View.GONE
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentСontainerView, SelectedTracks())
+                .setReorderingAllowed(true)
+                .commit()
+        }
+        binding.playlists.setOnClickListener {
+            fragmentSelectedFlag = FragmentSelectionFlag.PLAYLISTS
+            binding.selectedTracksLine.visibility = View.GONE
+            binding.playlistsLine.visibility = View.VISIBLE
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentСontainerView, Playlists())
+                .setReorderingAllowed(true)
+                .commit()
+        }
         binding.backToMainFromLibrary.setOnClickListener {
             val butBackClickListener = Intent(context, MainActivity::class.java)
             butBackClickListener.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
             this.finish()
         }
+
     }
+
+
 
 }
--- app/src/main/java/com/practicum/playlistmaker/player/ui/activity/Playlists.kt
+++ app/src/main/java/com/practicum/playlistmaker/player/ui/activity/Playlists.kt
@@ -1,4 +1,4 @@
-package com.practicum.playlistmaker.library.ui.activity
+package com.practicum.playlistmaker.player.ui.activity
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,19 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.practicum.playlistmaker.databinding.FragmentPlaylistsBinding
-import com.practicum.playlistmaker.library.ui.viewModel.PlaylistsViewModel
+import com.practicum.playlistmaker.databinding.FragmentSelectedTracksBinding
 import com.practicum.playlistmaker.search.domain.Track
-import org.koin.android.ext.android.inject
 
-class PlaylistsFragment : Fragment() {
+class Playlists : Fragment() {
     //переменная-заглушка, тип данных не тот
     private val  playlists:ArrayList<Track>?=null
-
     private var _binding: FragmentPlaylistsBinding? = null
     // создаём неизменяемую переменную, к которой можно будет обращаться без ?. Мы должны не забыть инициализировать _binding, до того как использовать
     private val binding get() = _binding!!
-
-    private val viewModel: PlaylistsViewModel by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,17 +29,5 @@ class PlaylistsFragment : Fragment() {
             binding.errorNoSelectedTracks.visibility = View.GONE
         }
         return binding.root
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        // реализация появится в следующем  спринте
-    }
-
-
-    companion object {
-        fun newInstance() = PlaylistsFragment().apply {
-            // позже сюда будет передаваться список плейлистов
-
-        }
     }
 }
